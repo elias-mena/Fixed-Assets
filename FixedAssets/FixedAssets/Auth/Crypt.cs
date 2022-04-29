28ab8cc2e1d6b4406d9badcda10da0123574e423
homerox <eliasmena.2911@gmail.com>
2022-04-29 02:39:59 -0600

    Methods to encrypt and unencrypt passwords

---
+++ FixedAssets/FixedAssets/Auth/Crypt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace FixedAssets.Auth
+{
+    public class Crypt
+    {
+        public string Encrypt(string password)
+        {
+            return Convert.ToBase64String(new UnicodeEncoding().GetBytes(password));
+        }
+        public string Unencrypt(string password)
+        {
+            return new UnicodeEncoding().GetString(Convert.FromBase64String(password));
+        }
+    }
+}
