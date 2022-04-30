c42d6fcb1ce8ad2bc4229e3d9dae8770b579300b
homerox <eliasmena.2911@gmail.com>
2022-04-30 04:33:15 -0600

    Delete Asset Form

--- FixedAssets/FixedAssets/Assets/DeleteAsset.aspx.cs
+++ FixedAssets/FixedAssets/Assets/DeleteAsset.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Odbc;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,6 +13,32 @@ namespace FixedAssets.Assets
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void ButtonDelete_Click(object sender, EventArgs e)
+        {
+            SqlDataSource1.DeleteCommand = $"delete from assets where id = '{TextBoxId.Text}'";
+            int cant = SqlDataSource1.Delete();
+            if (cant == 1)
+                Label1.Text = "Activo eliminado!";
+            else
+                Label1.Text = "No existe activo con ese id ";
+            TextBoxId.Text = "";
+            LabelNombre.Text = "";
+        }
+
+        protected void ButtonSearch_Click(object sender, EventArgs e)
+        {
+            SqlDataSource1.SelectCommand = $"select * from assets where id='{TextBoxId.Text}'";
+            SqlDataSource1.DataSourceMode = SqlDataSourceMode.DataReader;
+            OdbcDataReader datos = (OdbcDataReader)SqlDataSource1.Select(DataSourceSelectArguments.Empty);
+
+            if (datos.Read())
+            {
+                LabelNombre.Text = (string)datos["name"];
+            }
+            else
+                Label1.Text = "No existe activo con ese id";
         }
     }
 }
